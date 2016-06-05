319c510fcf6c2bb374a5f1c88ab79a23540c4224
firefly1010 <wenzel.joubert@gmail.com>
2016-06-05 23:10:31 +0800

    working location adds

--- app/views/main/main-page.js
+++ app/views/main/main-page.js
@@ -2,11 +2,21 @@ var frames = require("ui/frame");
 var platform = require("platform");
 var vmModule = require("./main-view-model");
 var utilityModule = require("utils/utils");
+var geolocation = require("nativescript-geolocation");
+var observable = require("data/observable");
+var observableArray = require("data/observable-array");	
+var http = require("http");
 
 var twoPaneLayout = Math.min(platform.screen.mainScreen.widthDIPs, platform.screen.mainScreen.heightDIPs) > 600;
+
+var viewModel = new observable.Observable();
+var items = new observableArray.ObservableArray();
+
 function pageLoaded(args) {
     var page = args.object;
-    page.bindingContext = vmModule.mainViewModel;
+    //page.bindingContext = vmModule.mainViewModel;
+    viewModel.set("items", items);
+    page.bindingContext = viewModel;
 }
 exports.pageLoaded = pageLoaded;
 
@@ -17,6 +27,35 @@ exports.website = function() {
 exports.facebook = function() {
     console.log('launch facebook');
     utilityModule.openUrl("https://www.facebook.com/people/Gerrie-Kirton/100009234595792");
+}
+
+exports.deals = function(args) {
+    console.log('launch deals');
+    var page = args.object;
+    function assignItems(data){
+        if(data.content.toJSON().length > 0)
+         // items = new observableArray.ObservableArray();
+        data.content.toJSON().forEach(function(item) {
+            items.push(item);
+        }, this);
+    }
+    geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
+    then(function(possition){
+        var pos = JSON.stringify(possition)
+        console.log(pos);
+        http.request ({url:"http://ckgapi.azurewebsites.net/api/advert/getPositionAdverts", method:"POST", content: pos}).then(function(result) {
+            console.log('Succesfully got data from api');
+            assignItems(result)
+        }, function(error) {
+            console.error(JSON.stringify(error));
+        })
+        
+
+        // could request all adverts for this location
+        myLocation = possition;
+
+    console.log('latitude: ' + possition.latitude);	
+    })
 }
 
 function listViewItemTap(args) {
--- app/views/main/main-view-model.js
+++ app/views/main/main-view-model.js
@@ -19,22 +19,17 @@ var ViewModelItem = (function () {
 }());
 exports.ViewModelItem = ViewModelItem;
 var items = new observableArray.ObservableArray();
-// items.push(new ViewModelItem("http://mysmarthome.cloudapp.net/images/CKGLogo.jpg", "http://mysmarthome.cloudapp.net/images/vouchers/voucher1.png"));
-// items.push(new ViewModelItem("http://mysmarthome.cloudapp.net/images/image1.jpg", "http://mysmarthome.cloudapp.net/images/vouchers/voucher1.png"));
-// items.push(new ViewModelItem("http://mysmarthome.cloudapp.net/images/image2.jpg", "http://mysmarthome.cloudapp.net/images/vouchers/voucher3.jpg"));
 
-function assignItems(data){
-    data.forEach(function(item) {
-        items.push(new ViewModelItem(item.longitude, item.latitude, item.image, item.voucher, item.startdate, item.enddate, item.name))
-    }, this);
-}
+// function assignItems(data){
+//     if(data.content.toJSON().length > 0)
+//     data.content.toJSON().forEach(function(item) {
+//         console.log(item);
+//         items.push(new ViewModelItem(item.longitude, item.latitude, item.image, item.voucher, item.startdate, item.enddate, item.name))
+//         console.log(item.name);
+//     }, this);
+// }
 
-  http.getJSON("http://ckgapi.azurewebsites.net/api/advert/getAdverts").then(function(result) {
-        console.log('Succesfully got data from api');
-         assignItems(result)
-    }, function(error) {
-        console.error(JSON.stringify(error));
-    })
+
 
 // http.fetch("http://ckgapi.azurewebsites.net/api/advert/getAdverts", {
 //     method: "GET"
@@ -54,14 +49,33 @@ var myLocation = new observable.Observable();
 //var myLocation;
 //console.log(geolocation);
 
-geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
-then(function(possition){
+// var getDeals = function (){
+//     geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
+//     then(function(possition){
+//         var pos = JSON.stringify(possition)
+//         console.log(pos);
+//         http.request ({url:"http://ckgapi.azurewebsites.net/api/advert/getPositionAdverts", method:"POST", content: pos}).then(function(result) {
+//             console.log('Succesfully got data from api');
+//             assignItems(result)
+//         }, function(error) {
+//             console.error(JSON.stringify(error));
+//         })
+        
+//         //   http.getJSON("http://ckgapi.azurewebsites.net/api/advert/getAdverts").then(function(result) {
+//         //     console.log('Succesfully got data from api');
+//         //      assignItems(result)
+//         // }, function(error) {
+//         //     console.error(JSON.stringify(error));
+//         // })
 
-	// could request all adverts for this location
-	myLocation = possition;
+//         // could request all adverts for this location
+//         myLocation = possition;
 
-  console.log('latitude: ' + possition.latitude);	
-})
+//     console.log('latitude: ' + possition.latitude);	
+//     })
+// }
+
+
 exports.mainViewModel = new observable.Observable();
 exports.mainViewModel.set("items", items);
 exports.mainViewModel.set("myLocation", myLocation);
